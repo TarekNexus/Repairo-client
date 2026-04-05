3a95ea96f706be265c6761e521723e39f1cb369f
TarekNexus <muhammadtarek5920@gmail.com>
2026-04-05 16:36:14 +0600

    home page layout updaetd

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,32 +1,26 @@
-
-
-
-import FaqSection from '@/components/home/FAQSection';
-import Features from '@/components/home/Features';
-import Banner from '@/components/home/Hero';
-import MostPopular from '@/components/home/MostPopular';
-import Testimonials from '@/components/home/Testimonials';
+import FaqSection from "@/components/home/FAQSection";
+import Features from "@/components/home/Features";
+import Banner from "@/components/home/Hero";
+import MostPopular from "@/components/home/MostPopular";
+import Testimonials from "@/components/home/Testimonials";
 // import Sticky from '@/components/home/Sticky';
-import React from 'react';
+import React from "react";
 
-const page = async() => {
-
-    return (
-       
-          <div className="relative">
+const page = async () => {
+  return (
+    <div className="relative">
       <div className="z-1 sticky top-0 isolate">
         <Banner />
       </div>
       <div className="relative z-10 bg-white rounded-t-[40px]">
-    
-           <MostPopular/>
-           {/* <Sticky></Sticky> */}
-           <Features/>
-           <Testimonials></Testimonials>
-           <FaqSection></FaqSection>
+        <MostPopular />
+        {/* <Sticky></Sticky> */}
+        <Features />
+        <Testimonials></Testimonials>
+        <FaqSection></FaqSection>
       </div>
     </div>
-    );
+  );
 };
 
 export default page;
