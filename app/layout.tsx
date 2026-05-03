a4c4840fad5cd553e8d01c6a293d41f212ea24f4
TarekNexus <muhammadtarek5920@gmail.com>
2026-05-03 19:38:54 +0600

    code updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,15 +33,9 @@ export default function RootLayout({
      
      
 
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
             {children}
             <Toaster position="top-right" />
-          </ThemeProvider>
+         
       </body>
     </html>
   );
